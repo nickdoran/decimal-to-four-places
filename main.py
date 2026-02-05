8e126ca6403b4513544c75c7cea37fcc220c6c81
Nicklas Doran <nickdoran6@gmail.com>
2026-02-05 17:59:30 -0500

    Added feature where the rounded number is copied to users clipboard

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 def is_valid_input(value):
     """Check if input is 'quit' or a valid number (int or float)."""
     if value.lower() == "quit":
@@ -17,7 +19,13 @@ while True:
     if not is_valid_input(user_input):
         print("Invalid input. Please enter a number or 'quit'.")
         continue
-
-    print(round(float(user_input), 4))
+    rounded_number = round(float(user_input), 4)
+    str_rounded_number = str(rounded_number)
+    print(str_rounded_number)
+    try:
+        subprocess.run("pbcopy", text=True, input=str_rounded_number, check=True)
+        print("Copied to Clipboard")
+    except subprocess.CalledProcessError:
+        print("Failed to copy to clipboard")
 
 print("quitting application")
